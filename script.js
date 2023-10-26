c684e9fdb90beaed9db26bdc57966fc0bf4393ed
edwrddz6 <134966646+edwrddz6@users.noreply.github.com>
2023-10-26 10:54:27 -0700

    Add code for exercise

---
+++ script.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5];
+
+function calculateAverage(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+console.log(calculateAverage(arr));
